b836b6d6e15290f54b2368c467ea30df966864d9
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-02-19 10:30:10 +0700

    Test: add get checkups list by patient Id tests (TDD)

---
+++ HospitalCheckupSystem.Tests/Application/GetPatientCheckupsTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using HospitalCheckupSystem.Application.UseCases;
+using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+
+namespace HospitalCheckupSystem.Tests.Application;
+
+public class GetPatientCheckupsTests
+{
+    [Fact]
+    public async Task Should_Return_Checkups_For_Patient()
+    {
+        var patientId = Guid.NewGuid();
+
+        var checkups = new List<MedicalCheckup>
+        {
+            new MedicalCheckup(patientId, "MCU-2026-000001", DateTime.Today.AddDays(-1)),
+            new MedicalCheckup(patientId, "MCU-2026-000002", DateTime.Today)
+        };
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock.Setup(r => r.GetByPatientIdAsync(patientId))
+            .ReturnsAsync(checkups);
+
+        var useCase = new GetPatientCheckupsUseCase(repoMock.Object);
+
+        var result = await useCase.Execute(patientId);
+
+        result.Should().HaveCount(2);
+        result.First().McuNumber.Should().Be("MCU-2026-000002");
+    }
+
+    [Fact]
+    public async Task Should_Return_Empty_List_When_No_Checkups()
+    {
+        var patientId = Guid.NewGuid();
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock
+            .Setup(r => r.GetByPatientIdAsync(patientId))
+            .ReturnsAsync(new List<MedicalCheckup>());
+
+        var useCase = new GetPatientCheckupsUseCase(repoMock.Object);
+
+        var result = await useCase.Execute(patientId);
+
+        result.Should().BeEmpty();
+    }
+}
+
+}
