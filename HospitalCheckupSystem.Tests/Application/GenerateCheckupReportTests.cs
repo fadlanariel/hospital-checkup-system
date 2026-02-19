1b1333c49fb7318cd24017abc5db1ecccbdbd741
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-02-19 10:42:04 +0700

    test: add test for generate checkup report

---
+++ HospitalCheckupSystem.Tests/Application/GenerateCheckupReportTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using HospitalCheckupSystem.Application.UseCases;
+using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+using HospitalCheckupSystem.Tests.Helpers;
+using Moq;
+using Xunit;
+
+namespace HospitalCheckupSystem.Tests.Application;
+
+public class GenerateCheckupReportTests
+{
+    [Fact]
+    public async Task Should_Generate_Report_When_Checkup_Finished()
+    {
+        var checkup = CompletedCheckupFactory.CreateCompleted();
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock
+            .Setup(r => r.GetByIdAsync(checkup.Id))
+            .ReturnsAsync(checkup);
+
+        var useCase = new GenerateCheckupReportUseCase(repoMock.Object);
+
+        var result = await useCase.Execute(checkup.Id);
+
+        result.McuNumber.Should().Be(checkup.McuNumber);
+        result.PatientId.Should().Be(checkup.PatientId);
+        result.Conclusion!.Diagnosis.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Should_Throw_When_Checkup_Not_Finished()
+    {
+        var checkup = new MedicalCheckup(
+            Guid.NewGuid(),
+            "MCU-2026=000001",
+            DateTime.Today
+        );
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock
+            .Setup(r => r.GetByIdAsync(checkup.Id))
+            .ReturnsAsync(checkup);
+
+        var useCase = new GenerateCheckupReportUseCase(repoMock.Object);
+
+        var act = () => useCase.Execute(checkup.Id);
+
+        await act.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("Checkup not finished");
+    }
+
+}
