4f09ba867e178bd8f3c5704743d13b1fbe4da852
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-02-19 08:51:14 +0700

    test: add TDD for GetMedicalCheckupUseCase

---
+++ HospitalCheckupSystem.Tests/Application/GetMedicalCheckupTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using HospitalCheckupSystem.Application.UseCases;
+using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+
+namespace HospitalCheckupSystem.Tests.Application;
+
+public class GetMedicalCheckupTests
+{
+    [Fact]
+    public async Task Should_Return_Checkup_When_Exists()
+    {
+        var checkupId = Guid.NewGuid();
+
+        var checkup = new MedicalCheckup(
+            Guid.NewGuid(),
+            "MCU-2026-000001",
+            DateTime.Today
+        );
+
+        var repositoryMock = new Mock<IMedicalCheckupRepository>();
+        repositoryMock
+            .Setup(r => r.GetByIdAsync(checkupId))
+            .ReturnsAsync(checkup);
+
+        var useCase = new GetMedicalCheckupUseCase(repositoryMock.Object);
+
+        var result = await useCase.Execute(checkupId);
+
+        result.Should().NotBeNull();
+        result.Should().Be(checkup);
+    }
+
+    [Fact]
+    public async Task Should_Throw_When_Checkup_Not_Found()
+    {
+        var checkupId = Guid.NewGuid();
+
+        var repositoryMock = new Mock<IMedicalCheckupRepository>();
+        repositoryMock
+            .Setup(r => r.GetByIdAsync(checkupId))
+            .ReturnsAsync((MedicalCheckup?)null);
+
+        var useCase = new GetMedicalCheckupUseCase(repositoryMock.Object);
+
+        var act = () => useCase.Execute(checkupId);
+
+        await act.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("Checkup not found");
+    }
+}
