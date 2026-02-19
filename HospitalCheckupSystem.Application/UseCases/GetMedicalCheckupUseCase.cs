d04fc76149e104d3377624ef50c3a5d3aa7b0ff0
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-02-19 08:51:19 +0700

    feat: implement GetMedicalCheckupUseCase

---
+++ HospitalCheckupSystem.Application/UseCases/GetMedicalCheckupUseCase.cs
@@ -0,0 +1,24 @@
+﻿using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+
+namespace HospitalCheckupSystem.Application.UseCases;
+
+public class GetMedicalCheckupUseCase
+{
+    private readonly IMedicalCheckupRepository _repository;
+
+    public GetMedicalCheckupUseCase(IMedicalCheckupRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<MedicalCheckup> Execute(Guid id)
+    {
+        var checkup = await _repository.GetByIdAsync(id);
+
+        if (checkup == null)
+            throw new InvalidOperationException("Checkup not found");
+
+        return checkup;
+    }
+}
