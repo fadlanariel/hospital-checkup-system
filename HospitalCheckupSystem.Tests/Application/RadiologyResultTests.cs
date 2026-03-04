4c99ae30b4ec1ab03ad20c8b9f12c21dd75dbf90
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-03-04 09:26:09 +0700

    test: add radiology result use case test

---
+++ HospitalCheckupSystem.Tests/Application/RadiologyResultTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using HospitalCheckupSystem.Application.UseCases;
+using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+
+namespace HospitalCheckupSystem.Tests.Application;
+
+public class RadiologyResultTests
+{
+    [Fact]
+    public async Task Should_Save_Radiology_Result()
+    {
+        var checkupId = Guid.NewGuid();
+
+        var checkup = new MedicalCheckup(
+            Guid.NewGuid(),
+            "MCU-2026-000003",
+            DateTime.Today
+        );
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock.Setup(r => r.GetByIdAsync(checkupId))
+                .ReturnsAsync(checkup);
+
+        var useCase = new RecordRadiologyUseCase(repoMock.Object);
+
+        await useCase.ExecuteAsync(new RecordRadiologyCommand
+        {
+            CheckupId = checkupId,
+            Examination = "Thorax",
+            Findings = "CTR > 50%",
+            Impression = "Cardiomegaly",
+            RadiologistName = "dr. Aswin",
+            ExamDate = DateTime.Today
+        });
+
+        checkup.Radiology.Should().NotBeNull();
+        repoMock.Verify(r => r.UpdateAsync(checkup), Times.Once);
+    }
+}
