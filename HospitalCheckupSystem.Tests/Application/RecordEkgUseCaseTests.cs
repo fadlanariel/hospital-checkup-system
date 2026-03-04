cb44cca3c4532b2e5252e9557ef4bc7cbf5a83c2
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-03-04 09:36:22 +0700

    test: add record ekg use case test

---
+++ HospitalCheckupSystem.Tests/Application/RecordEkgUseCaseTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using HospitalCheckupSystem.Domain.Entities;
+using HospitalCheckupSystem.Domain.Interfaces;
+using Moq;
+
+namespace HospitalCheckupSystem.Tests.Application;
+
+public class RecordEkgUseCaseTests
+{
+    [Fact]
+    public async Task Should_Save_Ekg_Result()
+    {
+        var checkupId = Guid.NewGuid();
+
+        var checkup = new MedicalCheckup(
+            Guid.NewGuid(),
+            "MCU-2026-000011",
+            DateTime.Today
+        );
+
+        var repoMock = new Mock<IMedicalCheckupRepository>();
+        repoMock.Setup(r => r.GetByIdAsync(checkupId))
+                .ReturnsAsync(checkup);
+
+        var useCase = new RecordEkgUseCase(repoMock.Object);
+
+        await useCase.ExecuteAsync(new RecordEkgCommand
+        {
+            CheckupId = checkupId,
+            Rhythm = "Sinus Rhythm",
+            HeartRate = 72,
+            Axis = "Normal",
+            Impression = "Normal ECG",
+            DoctorName = "dr. Budi",
+            ExamDate = DateTime.Today
+        });
+
+        checkup.Ekg.Should().NotBeNull();
+        repoMock.Verify(r => r.UpdateAsync(checkup), Times.Once);
+    }
+}
